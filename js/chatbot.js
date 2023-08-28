9ff5b433bc99a9e847b3fce5f1451a40f16838cf
GonzaCC <acanazac@uni.pe>
2023-08-27 23:04:14 -0500

    Improved chatbot.js

--- js/chatbot.js
+++ js/chatbot.js
@@ -2,27 +2,28 @@ let inputMessage = document.getElementById('input-message');
 let sendMessage = document.getElementById('send-message');
 let chatMessages = document.querySelector('.chat-messages');
 const arrayMessages = [
-    {
-        id: 1,
-        user: 'user001',
-        message: 'Hello, its a test',
-    }
+    // {
+    //     id: 1,
+    //     user: 'user001',
+    //     message: 'Hello, its a test',
+    // }
 ];
 
 function readMessages() {
-    for(let i = 0; i < arrayMessages.length; i++) {
+    // for(let i = 0; i < arrayMessages.length; i++) {
          chatMessages.innerHTML += `
             <div class="message-user-1">
                 <img src="/images/user_1.png" alt="user-1">
                 <div class="message-user">
                     <p>
-                        What is design?
+                        ${arrayMessages[arrayMessages.length - 1].message}
                     </p>
                 </div>
                 <img src="/icons/edit.svg" alt="edit">
             </div>
+            <br>
          `;
-    }
+    // }
 };
 
 function addedMessage(message) {
@@ -35,7 +36,11 @@ function addedMessage(message) {
     );
 
     inputMessage.value = null;
+    readMessages();
+    console.log('mensaje agregado')
 };
+
+// readMessages();
 
 sendMessage.addEventListener('click', function() {
     addedMessage(inputMessage.value);
